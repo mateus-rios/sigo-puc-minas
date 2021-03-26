f7cf096b2da0ed6a72760972de465ea45f0503df
Mateus Rios <mateus.rios@interaconsultoria.com.br>
2021-03-25 21:59:52 -0300

    Update

---
+++ sigo-interface/src/app/home/home.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { SigninComponent } from './signin/signin.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { HttpClientModule } from '@angular/common/http';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSliderModule } from '@angular/material/slider'
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+
+@NgModule({
+    declarations: [SigninComponent, MainComponent],
+    imports: [
+        ReactiveFormsModule,
+        RouterModule,
+        HttpClientModule,
+        MatInputModule,
+        MatFormFieldModule,
+        BrowserAnimationsModule,
+        MatSliderModule,
+        MatSnackBarModule
+    ]
+})
+export class HomeModule {
+
+}
---
+++ sigo-interface/src/app/home/signin/signin.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/core/auth.service';
+import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+  templateUrl: './signin.component.html',
+  styleUrls: ['./signin.component.css']
+})
+export class SigninComponent implements OnInit {
+
+  loginForm: FormGroup
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private router: Router,
+    private snackBar: MatSnackBar
+  ) { }
+
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    })
+  }
+
+  login() {
+    if (this.loginForm.valid) {      
+      const credentials = this.loginForm.value
+      this.authService.authenticate(credentials)
+        .subscribe(
+          res => {
+            this.authService.saveToken(res)
+            this.router.navigate(['main'])
+          },
+          error => {
+            this.snackBar.open('Usuário ou senha inválidos. Verifique e tente novamente', 'OK')
+          }
+        )
+    }
+  }
+
+}
