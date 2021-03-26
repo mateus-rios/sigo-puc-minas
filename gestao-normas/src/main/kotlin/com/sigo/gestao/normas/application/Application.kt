f8d502bdb2cdf065549f689f935e710797699083
Mateus Rios <mateus.rios@interaconsultoria.com.br>
2021-03-26 02:05:42 -0300

    Update

--- gestao-normas/src/main/kotlin/com/sigo/gestao/normas/application/Application.kt
+++ gestao-normas/src/main/kotlin/com/sigo/gestao/normas/application/Application.kt
@@ -1,13 +1,29 @@
 package com.sigo.gestao.normas.application
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.client.RestTemplateBuilder
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
+import org.springframework.web.client.RestTemplate
 
 
 @SpringBootApplication
 @ComponentScan(basePackages = ["com.sigo.gestao.normas"])
-class Application
+class Application {
+
+	@Value("\${repositorio.normas.user}")
+	private lateinit var userRepositorioNormas: String
+
+	@Value("\${repositorio.normas.password}")
+	private lateinit var passwordRepositorioNormas: String
+
+	@Bean
+	fun restTemplate(restTemplateBuilder: RestTemplateBuilder): RestTemplate? {
+		return restTemplateBuilder.basicAuthentication(userRepositorioNormas, passwordRepositorioNormas).build()
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<Application>(*args)
--- gestao-normas/src/main/kotlin/com/sigo/gestao/normas/service/technical/standard/TechnicalStandardServiceImpl.kt
+++ gestao-normas/src/main/kotlin/com/sigo/gestao/normas/service/technical/standard/TechnicalStandardServiceImpl.kt
@@ -6,6 +6,7 @@ import com.sigo.gestao.normas.service.notfier.KafkaNotifier
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.web.client.RestTemplateBuilder
+import org.springframework.context.annotation.Bean
 import org.springframework.core.ParameterizedTypeReference
 import org.springframework.http.HttpMethod
 import org.springframework.http.HttpStatus
@@ -17,7 +18,7 @@ import java.util.UUID
 internal class TechnicalStandardServiceImpl
 @Autowired constructor(
         private val kafkaNotifier: KafkaNotifier,
-        restTemplateBuilder: RestTemplateBuilder
+        private val restTemplate: RestTemplate
 ) : TechnicalStandardService {
 
     private companion object {
@@ -25,16 +26,8 @@ internal class TechnicalStandardServiceImpl
     }
 
     @Value("\${repositorio.normas.url}")
-    private var urlRepositorioNormas: String = ""
+    private lateinit var urlRepositorioNormas: String
 
-    @Value("\${repositorio.normas.user}")
-    private var userRepositorioNormas: String = ""
-
-    @Value("\${repositorio.normas.password}")
-    private var passwordRepositorioNormas: String = ""
-
-    private val restTemplate: RestTemplate =
-            restTemplateBuilder.basicAuthentication(userRepositorioNormas, passwordRepositorioNormas).build()
 
     override fun create(technicalStandard: TechnicalStandard): TechnicalStandard {
         restTemplate.postForObject(urlRepositorioNormas, technicalStandard, Map::class.java)
@@ -54,4 +47,5 @@ internal class TechnicalStandardServiceImpl
         val response = restTemplate.exchange("$urlRepositorioNormas/all", HttpMethod.GET, null, object : ParameterizedTypeReference<List<TechnicalStandard>>() {})
         return response.body!!
     }
+    
 }
