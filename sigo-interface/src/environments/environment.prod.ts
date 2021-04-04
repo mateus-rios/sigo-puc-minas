956f43df3aa231b9f7e43e0cc14627f10cbe245f
Mateus Rios <mateus.rios@interaconsultoria.com.br>
2021-04-04 05:37:20 -0300

    Server URL

--- sigo-interface/src/environments/environment.prod.ts
+++ sigo-interface/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  gestaNormasUrl: 'http://localhost:8080/normas',
-  loginUrl: 'http://localhost:8080/auth'
+  gestaNormasUrl: 'http://35.168.74.18:8080/normas',
+  loginUrl: 'http://35.168.74.18:8080/auth'
 };
