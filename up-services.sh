a16079558a2c4cbed0a104d8caa07e8954ec7f5e
MATEUS SANTANA RIOS <mateus.rios@portoseguro.com.br>
2021-05-18 22:31:25 -0300

    Ajuste

--- up-services.sh
+++ up-services.sh
@@ -4,4 +4,4 @@ echo "setting up services (kafka)"
 docker-compose -f services-kafka/docker-compose.yaml up -d
 
 echo "creating topic"
-docker-compose exec kafka kafka-topics --create --topic gestao_normas --partitions 1 --replication-factor 1 --if-not-exists --zookeeper zookeeper:32181
+docker-compose -f services-kafka/docker-compose.yaml exec kafka kafka-topics --create --topic gestao_normas --partitions 1 --replication-factor 1 --if-not-exists --zookeeper zookeeper:32181
